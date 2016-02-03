e7710babc5bb398c42af3a0624a995de5f7b79ac
Tenghu Wu, PhD <wutenghu.ubc@gmail.com>
2016-02-02 17:20:24 -0800

    Update Simple_Security.m

--- Simple_Security.m
+++ Simple_Security.m
@@ -22,7 +22,7 @@ expReturn   = mean(returns); % calculate the mean return
 sigma       = std(returns); % calculate the standard deviations
 correlation = corrcoef(returns); % calculate the correlation coefficients
 t           = 0;
-X           = 100*ones(nVariables,1);
+X           = [prices(1,1);prices(1,2)]; % set the initial values
 
 %% Construct the SDE system
 F = @(t,X) diag(expReturn) * X;
@@ -32,16 +32,18 @@ SDE = sde(F, G, 'Correlation', ...
     correlation, 'StartState', X);
 
 %% Simulate the SDE system
-nPeriods = 249*2;      % # of simulated observations
+nPeriods = 252;      % # of simulated observations
 dt       =   1;      % time increment = 1 day
 rng(142857,'twister')
 
 [S,T] = simulate(SDE, nPeriods, 'DeltaTime', dt);
 
+TT = size(prices,1);
 %% Plot the figure
 plot(T, S), xlabel('Trading Day'), ylabel('Price')
+hold on
+plot(prices(:,:,1),'-.');
 title('First Path of Multi-Dimensional Market Model')
-legend({'Canada' 'France' 'Germany' 'Japan' 'UK' 'US'},...
-    'Location', 'Best')
+legend({'GOLD' 'ABX.TO'}, 'Location', 'Best')
 %%
 toc
